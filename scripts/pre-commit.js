6ed9747fb10b20ac83ce7b2b3ee48f82cd138a1a
Jonathan Tessman <jonathantessman@gmail.com>
2024-12-21 14:59:52 -0600

    Fixes to games.json

--- scripts/pre-commit.js
+++ scripts/pre-commit.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const GAMES_CONFIG = 'games.json';
+const GAMES_CONFIG = 'src/games.json';
 
 function getLastCommitHash(filePath) {
   try {
